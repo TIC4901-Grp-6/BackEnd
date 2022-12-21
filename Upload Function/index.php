6cfc83bbea0904a4ea2595a5978fed7eee965fab
WeiWei0721 <88671064+WeiWei0721@users.noreply.github.com>
2022-12-21 15:03:08 +0800

    pic upload function index file

--- Upload Function/index.php
+++ Upload Function/index.php
@@ -1,50 +1,65 @@
-<?php 
-// Include configuration file 
-include_once 'config.php'; 
- 
-$status = $statusMsg = ''; 
-if(!empty($_SESSION['status_response'])){ 
-    $status_response = $_SESSION['status_response']; 
-    $status = $status_response['status']; 
-    $statusMsg = $status_response['status_msg']; 
-     
-    unset($_SESSION['status_response']); 
-} 
+<?php
+error_reporting(0);
+
+$msg = "";
+
+// If upload button is clicked ...
+if (isset($_POST['upload'])) {
+
+	$filename = $_FILES["uploadfile"]["name"];
+	$tempname = $_FILES["uploadfile"]["tmp_name"];
+	$folder = "./image/" . $filename;
+
+	$db = mysqli_connect("localhost", "root", "mysql", "uploadimage");
+
+	// Get all the submitted data from the form
+	$sql = "INSERT INTO image (filename) VALUES ('$filename')";
+
+	// Execute query
+	mysqli_query($db, $sql);
+
+	// Now let's move the uploaded image into the folder: image
+	if (move_uploaded_file($tempname, $folder)) {
+		echo "<h3> Image uploaded successfully!</h3>";
+	} else {
+		echo "<h3> Failed to upload image!</h3>";
+	}
+}
 ?>
 
-<!-- Status message -->
-<?php if(!empty($statusMsg)){ ?>
-    <div class="alert alert-<?php echo $status; ?>"><?php echo $statusMsg; ?></div>
-<?php } ?>
+<!DOCTYPE html>
+<html>
 
-<!-- Upload form -->
-<div class="col-md-12">
-    <form method="post" action="upload.php" class="form" enctype="multipart/form-data">
-        <div class="form-group">
-            <label>Video File:</label>
-            <input type="file" name="file" class="form-control" required>
-        </div>
-        <div class="form-group">
-            <label>Title:</label>
-            <input type="text" name="title" class="form-control" required>
-        </div>
-        <div class="form-group">
-            <label>Description:</label>
-            <textarea name="description" class="form-control"></textarea>
-        </div>
-        <div class="form-group">
-            <label>Tags:</label>
-            <input type="text" name="tags" class="form-control">
-        </div>
-        <div class="form-group">
-            <label>Privacy:</label>
-            <select name="privacy" class="form-control">
-                <option value="public">Public</option>
-                <option value="private">Private</option>
-            </select>
-        </div>
-        <div class="form-group">
-            <input type="submit" class="form-control btn-primary" name="submit" value="Upload">
-        </div>
-    </form>
-</div>
+<head>
+	<title>Image Upload</title>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+
+<body>
+	<div id="content">
+		<form method="POST" action="" enctype="multipart/form-data">
+			<div class="form-group">
+				<input class="form-control" type="file" name="uploadfile" value="" />
+			</div>
+			<div class="form-group">
+				<button class="btn btn-primary" type="submit" name="upload">UPLOAD</button>
+			</div>
+		</form>
+	</div>
+	<div id="display-image">
+		<?php
+		$query = " select * from image ";
+		$result = mysqli_query($db, $query);
+
+		while ($data = mysqli_fetch_assoc($result)) {
+		?>
+			<img src="./image/<?php echo $data['filename']; ?>">
+
+		<?php
+		}
+		?>
+	</div>
+</body>
+
+</html>
